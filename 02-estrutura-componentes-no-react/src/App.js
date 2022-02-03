5c48edca71ae725d6bbeeb0e90efb5ca1b408a35
profoswaldo <83677370+profoswaldo@users.noreply.github.com>
2022-02-03 08:24:40 -0300

    Aula

---
+++ 02-estrutura-componentes-no-react/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+  {
+    emoji: '😀',
+    name: "test grinning face"
+  },
+  {
+    emoji: '🎉',
+    name: "party popper"
+  },
+  {
+    emoji: '💃',
+    name: "woman dancing"
+  }
+];
+
+function App() {
+  const greeting = "greeting";
+  const displayAction = false;
+  return(
+    <div className="container">
+      <h1 id={greeting}>Hello, World</h1>
+      {displayAction && <p>I am writing JSX</p>}
+      <ul>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button
+                onClick={displayEmojiName}
+              >
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App;
